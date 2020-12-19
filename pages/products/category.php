cd96bf371b0c1b0b3449d9f966ee25269e1a369a
mohammad-de <64242986+mohammad-de@users.noreply.github.com>
2020-12-19 18:03:15 +0100

    Edit #7

--- pages/products/category.php
+++ pages/products/category.php
@@ -94,6 +94,7 @@ $products             = $products_json->products;
 <head>
   <title>Suche</title>
   <meta charset="utf-8">
+  <link rel="icon" type="image/png" href="../../favicon.png"/>
   <!-- Import Google's Roboto font -->
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
--- pages/products/product.php
+++ pages/products/product.php
@@ -24,6 +24,7 @@ $categories                  = $helper->getCategories(false);
 <head>
   <title><?php echo $product->title ?></title>
   <meta charset="utf-8">
+  <link rel="icon" type="image/png" href="../../favicon.png"/>
   <!-- Import Google's Roboto font -->
   <link rel="preconnect" href="https://fonts.gstatic.com">
   <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
